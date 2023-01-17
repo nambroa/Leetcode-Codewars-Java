c92568d1658b25e59a4b343ddaea6c10ca697ca8
Nicolas Ambroa <ambroanicolas@gmail.com>
2023-01-17 00:55:25 -0300

    Add LeetCode 226 Invert Binary Tree solution and TreeNode LeetCode definition.

---
+++ LeetCode/InvertBinaryTree.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import LeetCode.helpers.TreeNode;
+
+/*
+226. Invert Binary Tree
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
