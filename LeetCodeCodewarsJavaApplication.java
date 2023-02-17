634dd42b2400215c2daa00c7c12a408aa0a941e5
Nicolas Ambroa <ambroanicolas@gmail.com>
2023-02-17 19:33:09 -0300

    Add CodeWars PickPeaks Kata solution.

--- LeetCodeCodewarsJavaApplication.java
+++ LeetCodeCodewarsJavaApplication.java
@@ -1,5 +1,6 @@
 import CodeWars.BuildTower;
 import CodeWars.EqualSidesOfArray;
+import CodeWars.PickPeaks;
 import CodeWars.TakeATenMinuteWalk;
 
 import java.util.Arrays;
@@ -7,6 +8,6 @@ import java.util.Arrays;
 public class LeetCodeCodewarsJavaApplication {
     public static void main(String[] args) {
         System.out.println("===================================== MAIN METHOD EXECUTING =====================================");
-        System.out.println(Arrays.toString(BuildTower.towerBuilder(3)));
+        PickPeaks.getPeaks(new int[]{3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3});
     }
 }
